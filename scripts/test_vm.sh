e5946b4cfffe90dbc2b3ff689228369fe9353ead
Jin Xi <jinxi.noogler@gmail.com>
2026-02-26 17:46:36 -0800

    Update test_vm.sh with default IP

--- scripts/test_vm.sh
+++ scripts/test_vm.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ -z "$1" ]; then
-  echo "Usage: ./test_vm.sh http://<VM_IP>:8080"
-  exit 1
-fi
+BASE_URL=${1:-"http://34.87.14.235:8080"}
 
-BASE_URL=$1
 
 # We can reuse the same test script logic by delegating to test_local.sh
 # just passing the base URL
